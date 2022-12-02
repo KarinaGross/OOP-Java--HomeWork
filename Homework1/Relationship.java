6970e1936ff34c17224eeec9a4d96ec4860e10f0
Karina <karina.dragan4uk@yandex.ru>
2022-12-02 21:27:15 +0300

    ДЗ_3. Класс Relationship заменен одноименным enum

--- Homework1/Relationship.java
+++ Homework1/Relationship.java
@@ -1,45 +1,6 @@
-// public class Relationship {
-//     private static String children = "children";
-//     private static String parents = "parents";
-//     private static String siblings = "siblings";
-//     private static String partner = "partner";
-
-//     public static String parents() {
-//         return parents;
-//     }
-
-//     public static String children() {
-//         return children;
-//     }
-
-//     public static String siblings() {
-//         return siblings;
-//     }
-    
-//     public static String partner() {
-//         return partner;
-//     }
-// }
-
 public enum Relationship {
     CHILDREN,
     PARENTS,
     SIBLINGS,
     PARTNER;
-
-    // CHILDREN("children"),
-    // PARENTS("parents"),
-    // SIBLINGS("siblings"),
-    // PARTNER("partner");
-
-    // private String relation;
-
-    // private Relationship(String relation) {
-    //     this.relation = relation;
-    // }
-
-    // @Override
-    // public String toString() {
-    //     return relation;
-    // }
 }
