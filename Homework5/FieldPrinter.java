cea602d7c0b5732b93ae2813b37d69e2a7c5fe1c
Karina <karina.dragan4uk@yandex.ru>
2022-12-12 17:55:38 +0300

    решено

---
+++ Homework5/FieldPrinter.java
@@ -0,0 +1,42 @@
+package Homework5;
+
+
+public class FieldPrinter {
+    // PlayingField field;
+
+    public String printColorField(PlayingField field) {
+        // this.field = field;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < field.getRowLen(); i++) {
+            for (int j = 0; j < field.getColLen(); j++) {
+                if (field.getCountPoint(i, j) == -1) {
+                    result.append("▓▓");
+                } else if (field.getCountPoint(i, j) == -2) {
+                    result.append("▒▒"); //
+                } else {
+                    result.append("░░");
+                }
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+
+    public String printFieldOfNumbers(PlayingField field) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < field.getRowLen(); i++) {
+            for (int j = 0; j < field.getColLen(); j++) {
+                if (String.valueOf(field.getCountPoint(i, j)).length() > 1) {
+                    result.append(field.getCountPoint(i, j) + " ");
+                } else {
+                    result.append(" " + field.getCountPoint(i, j)  + " ");
+                }
+                
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+}
